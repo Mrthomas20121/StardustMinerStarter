dcc9c23eddbff6d92921059bb401c65297d5a455
Thomas BLAVET <@infogestion.local>
2019-04-29 11:52:19 +0200

    config, main prog and assembly info

---
+++ StardustMiner/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace StardustMiner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process java = new Process();
+            java.StartInfo.FileName = AppDomain.CurrentDomain.BaseDirectory + "jre/bin/java.exe";
+            java.StartInfo.Arguments = "-jar ./bin/stardust.jar";
+            java.Start();
+        }
+    }
+}
